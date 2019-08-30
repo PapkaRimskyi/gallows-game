a41622249462d83ab0757cd08018d05ee0457078
Гуров Георгий <magnezia98@mail.ru>
2019-08-30 03:16:21 +0300

    Symbol appears, if user will choose right symbol

--- source/js/js_module/renderGameZone.js
+++ source/js/js_module/renderGameZone.js
@@ -2,8 +2,11 @@
   const letterListContainer = document.querySelector('.play-area__letter-list-container');
   const startGameButton = document.querySelector('.game-header__start-game');
   const healthBar = document.querySelectorAll('.play-area__attempts-left-health');
-  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  const alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя';
   const wordArray = ['дружба', 'пляж', 'солнце', 'луна', 'спутник', 'механизм', 'человек'];
+  const gameInfoObj = {};
+
+  //Game zone render code
 
   function healthColor(color) {
     for (let key of healthBar) {
@@ -36,13 +39,14 @@
       letterListContainer.append(ul);
     }
     renderLetterCells();
+    console.log(gameInfoObj.word);
   }
 
-  startGameButton.addEventListener('click', renderLetterList);
-
   function renderLetterCells() {
-    let randomWord = wordArray[Math.floor(Math.random() * wordArray.length)].toUpperCase();
     if (!document.querySelector('.play-area__hidden-word-list')) {
+      let randomWord = wordArray[Math.floor(Math.random() * wordArray.length)].toUpperCase();
+      gameInfoObj.word = randomWord;
+      gameInfoObj.attempts = [true, true, true, true, true, true];
       let ul = document.createElement('ul');
       ul.classList.add('play-area__hidden-word-list');
       for (let i = 0; i < randomWord.length; i++) {
@@ -55,5 +59,23 @@
       document.querySelector('.play-area__human-figure').style = 'display: none;';
     }
   }
+
+  startGameButton.addEventListener('click', renderLetterList);
+
+  //Game code
+
+  function buttonLetterHandler(evt) {
+    let target = evt.target;
+    let hiddenWordCell = document.querySelectorAll('.play-area__hidden-word-list-item');
+    if (target.tagName === 'BUTTON') {
+      for (let i = 0; i < gameInfoObj.word.length; i++) {
+        if (target.textContent === gameInfoObj.word[i]) {
+          hiddenWordCell[i].textContent = target.textContent;
+        }
+      }
+    }
+  }
+
+  letterListContainer.addEventListener('click', buttonLetterHandler);
 
 })();
